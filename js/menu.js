7940b226428c9267641525674e8984052a8c606a
Johan <johan@petfactory.se>
2020-04-30 13:48:36 +0200

    rebuilding site Thu, Apr 30, 2020  1:48:36 PM

--- js/menu.js
+++ js/menu.js
@@ -7,18 +7,15 @@ $(document).ready(function(){
         // console.log(12);
     });
 
-
-    /*
-    $(window).scroll(function(){
-    	
-    	var fade_height = 470;
-    	var offset = 0;
-    	// var alpha = Math.min(.75, (($(window).scrollTop()-offset) / fade_height));
-        var alpha = Math.min(1.0, (($(window).scrollTop()-offset) / fade_height));
-    	console.log(alpha, $(window).scrollTop())
-    	$("#header-bg-fade").css("opacity", alpha);
-  	});
-    */
+   //  $(window).scroll(function(){	
+   //  	var fade_height = 400;
+   //  	var offset = 0;
+   //  	// var alpha = Math.min(.75, (($(window).scrollTop()-offset) / fade_height));
+   //      var alpha = Math.min(1.0, (($(window).scrollTop()-offset) / fade_height));
+   //  	// console.log(alpha, $(window).scrollTop())
+   //  	$(".site-header").css("opacity", alpha);
+  	// });
+    
 
 });
 
