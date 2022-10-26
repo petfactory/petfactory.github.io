7a7afbd1757206696d94d493ba795c39a05c8678
Johan <johan@petfactory.se>
2022-10-26 12:05:48 +0200

    rebuilding site 26 Oct 2022 12:05:48

--- js/menu.js
+++ js/menu.js
@@ -50,6 +50,30 @@ document.addEventListener('DOMContentLoaded', function() {
         }, false);
     });
 
+    const clipboard_expand_buttons = document.querySelectorAll('.clipboard-snippet-expand-btn');
+
+    clipboard_expand_buttons.forEach(expand_btn => {
+
+        expand_btn.addEventListener('click', function(e) {
+            // var data = expand_btn.dataset.clipboard_snippet;
+
+            // var parent_div = expand_btn.parentNode;
+
+            var vex_div = expand_btn.parentNode.getElementsByClassName("vex-snippet")[0];
+            console.log(vex_div);
+
+            if(vex_div.style.display == "block") {
+            vex_div.style.display = "none";
+            }
+            else {
+                vex_div.style.display = "block";
+            }
+
+            // navigator.clipboard.writeText(data);
+            // console.log(data);
+        }, false);
+    });
+
 
     // mobile menu --------------------------
 
